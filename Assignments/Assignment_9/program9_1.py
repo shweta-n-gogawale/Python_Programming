36019eb8cd57fbb30c5f13e9da421b1b3434948d
Shweta Gogawale <gogawaleshweta12@gmail.com>
2026-01-27 10:58:36 +0530

    Add Assignment 9 solutions (Q1–Q5)

---
+++ Assignments/Assignment_9/program9_1.py
@@ -0,0 +1,13 @@
+"""
+    Function Name   : Display
+    Description     : Prints "Jay Ganesh" on console.
+    Input           : None
+    Output          : Jay Ganesh
+    Author          : Shweta Narayan Gogawale
+    Date            : 2026-01-27
+"""
+
+def Display():
+    print("Jay Ganesh")
+
+Display()
